f5e5b27405c6fb2dfa56872671a45c1982a5ff41
Ken <kchylee@gmail.com>
2017-04-21 20:44:28 +0000

    MongoDB connected to tweeter. Writing and storing tweets tested and working.

--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -10,6 +10,7 @@ module.exports = function(DataHelpers) {
   tweetsRoutes.get("/", function(req, res) {
     DataHelpers.getTweets((err, tweets) => {
       if (err) {
+        console.log("This is the beginning of the if statement")
         res.status(500).json({ error: err.message });
       } else {
         res.json(tweets);
